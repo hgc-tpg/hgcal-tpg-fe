1bfd2df8fae8b8fe85f288bd432ff3221339e9c2
Indranil Das <indra.ehep@gmail.com>
2025-09-15 18:14:30 +0200

    Updated dat link

--- setup.sh
+++ setup.sh
@@ -36,7 +36,7 @@ if [ -L dat ] ; then
 fi
 
 if [ $islxplus -eq 1 ] ; then
-    ln -s /eos/cms/store/group/dpg_hgcal/tb_hgcal/2024/BeamTestAug/HgcalBeamtestAug2024 dat
+    ln -s /eos/cms/store/group/dpg_hgcal/tb_hgcal/2024/BeamTestSep/HgcalBeamtestSep2024 dat
 fi
 if [ $ishgcbeamtestpc -eq 1 ] ;  then
     ln -s /till/HgcalBeamtest2024_TEST dat
